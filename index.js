030e7ac6a865e55b27f3a67576bda209d03fccde
minnehkamau <minnieches08@gmail.com>
2022-12-01 14:34:27 +0300

    Complete assingment

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Write your solution here!
+let cats = ["Milo", "Otis","Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph)
+  }
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob)
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield)
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo)
+}
+function appendCat(Broom){
+    let newCatList = cats.slice(-3)
+    newCatList.push(Broom)
+    return newCatList
+}
+function prependCat(Arnold){
+    let newCatList = cats.slice(0)
+    newCatList.unshift(Arnold)
+    return newCatList
+}
+function removeLastCat(Garfield){
+    let newCatList = cats.slice(-3)
+        newCatList.pop(Garfield)
+    return newCatList
+}
+function removeFirstCat(Milo){
+    let newCatList = cats.slice(0)
+    newCatList.shift(Milo)
+    return newCatList
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
